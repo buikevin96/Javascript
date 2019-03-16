9f4a93377b0d3453312e6b176ca7b8c6530f139b
buikevin96 <buikevin96@gmail.com>
2019-03-16 08:50:56 -0400

    Conditionals, functions, arrow functions, constructor functions

--- 1/main.js
+++ 1/main.js
@@ -45,6 +45,7 @@ console.warn("This is a warning");
     // person.email = "John@gmail.com"; // Adds email to person object
     // console.log(person);
 
+ /*   
         const todos = [
             { 
                 id: 1,
@@ -100,6 +101,73 @@ for (let todo of todos){
 
     const todoCompleted = todos.filter(function(todo){
         return todo.isCompleted === true;
+    }).map(function(todo){
+        return todo.text;
     });
 
     console.log(todoCompleted);
+*/
+
+/*
+const x = 10;
+const y = 10;
+
+if (x > 5 || y > 10) {
+    console.log('x is more than 5 or y is more than 10');
+} else if (x > 10){
+    console.log("x is greater than 10")
+}   else {
+    console.log("x is NOT 10");
+}
+
+*/
+
+/*
+const x = 10;
+
+// Ternary operator
+// if x is greater than 10, then set to red, else, blue
+    const color = x > 10 ? 'red' : ; 'blue';
+
+    switch(){
+        case "red":
+            console.log("color is red");
+            break;
+        case "blue":
+            console.log("color is blue");
+            break;
+        default:
+            console.log("Color is not red or blue");
+            break;
+    }
+*/
+
+/*
+// Functions
+
+    function addNums(num1, num2) =>{
+        return (num1 + num2);
+    }
+
+    console.log(addNums(5,4));
+*/
+
+// Constructor Function
+function Person(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    // method
+    this.getBirthYear = function(){
+        return this.dob.getFullYear();
+    }
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+}
+
+// Instantiate object
+const person1 = new Person("Kevin", "Bui", "2-10-1996");
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
